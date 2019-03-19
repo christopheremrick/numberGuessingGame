05622e193897b4d0d0ea7b6d625e067079c4357c
Chris <christopher.emrick.2022@student.lhschools.org>
2019-03-19 13:38:46 -0400

    Created do while statement and created a few variables

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,25 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	//Using do while statement to loop until boolean is set to true
+
+        Integer Number = (int) (Math.random() * 100);
+        Scanner scan = new Scanner(System.in);
+
+        Boolean loop = true;
+        do {
+            System.out.println("Please guess a number between one and one hundred.");
+
+            
+
+
+
+
+        } while (loop);
+
     }
 }
