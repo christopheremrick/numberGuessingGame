3fc9ffd65152163b59b2bb02a57f1e425cc43d9f
Chris <christopher.emrick.2022@student.lhschools.org>
2019-03-19 13:39:18 -0400

    Changed to lowercase

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -7,13 +7,12 @@ public class Main {
     public static void main(String[] args) {
 	//Using do while statement to loop until boolean is set to true
 
-        Integer Number = (int) (Math.random() * 100);
+        Integer number = (int) (Math.random() * 100);
         Scanner scan = new Scanner(System.in);
 
         Boolean loop = true;
         do {
             System.out.println("Please guess a number between one and one hundred.");
-
             
 
 
